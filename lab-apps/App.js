8875dd710c6996493ec8432e4a313c59da2540ed
HZie <boss.hz734@gmail.com>
2022-03-30 15:19:09 +0900

    until chapter 4

--- lab-apps/App.js
+++ lab-apps/App.js
@@ -1,3 +1,456 @@
+//ch04 lab04 - drawer navigator
+import React, { useState } from 'react';
+import { Text, View, Button, TextInput, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const text_st = {
+  fontSize: 20,
+  margin: 10,
+};
+
+const input_style = { fontSize: 30, borderWidth: 1, padding: 10, margin: 10 };
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="About" component={AboutScreen} />
+        <Drawer.Screen name="Hello" component={HelloScreen} />
+        <Drawer.Screen name="구구단" component={MultiplicationScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function MultiplicationScreen() {
+  let [x, setX] = useState(0);
+  let [y, setY] = useState(0);
+  const [result, setResult] = useState(x * y);
+
+  return (
+    <View style={{ paddingTop: 30 }}>
+      <View style={{ paddingTop: 30 }}>
+        <View style={{ flexDirection: 'row' }}>
+          <Text style={text_st}>
+            {x} X {y} = {result}
+          </Text>
+        </View>
+      </View>
+
+      <View style={{ flexDirection: 'row' }}>
+        <View style={{ flexDirection: 'column' }}>
+          <Button
+            title="  +  "
+            onPress={() => {
+              x++;
+              setX(x);
+              setResult(x * y);
+            }}
+          />
+          <Button
+            title="  -  "
+            onPress={() => {
+              x--;
+              setX(x);
+              setResult(x * y);
+            }}
+          />
+        </View>
+
+        <View style={{ flexDirection: 'column' }}>
+          <Button
+            title="  +  "
+            onPress={() => {
+              y++;
+              setY(y);
+              setResult(x * y);
+            }}
+          />
+          <Button
+            title="  -  "
+            onPress={() => {
+              y--;
+              setY(y);
+              setResult(x * y);
+            }}
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+function HelloScreen() {
+  const [greet, setGreet] = useState('Hello');
+  const [name, setName] = useState('User');
+
+  return (
+    <View style={{ paddingTop: 30 }}>
+      <Text style={text_st}>
+        {greet}, {name}
+      </Text>
+      <TextInput style={input_style} onChangeText={setName} />
+      <View style={{ margin: 10, flexDirection: 'row-reverse' }}>
+        <Button
+          title="Nice"
+          onPress={() => {
+            setGreet('Nice to meet you');
+          }}
+        />
+        <Button
+          title="Hello"
+          onPress={() => {
+            setGreet('Hello');
+          }}
+        />
+      </View>
+    </View>
+  );
+}
+
+function AboutScreen() {
+  return (
+    <View>
+      <Text style={text_st}>This is about the app</Text>
+      <Image
+        style={{ width: 200, height: 200 }}
+        source={require('./assets/cat-icon.png')}
+      />
+    </View>
+  );
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <View>
+      <Text style={text_st}>Home Screen</Text>
+      <Button
+        title="About"
+        onPress={() => {
+          navigation.navigate('About');
+        }}
+      />
+      <View style={{ height: 10 }} />
+      <Button
+        title="Hello"
+        onPress={() => {
+          navigation.navigate('Hello');
+        }}
+      />
+
+      <Button
+        title="구구단"
+        onPress={() => {
+          navigation.navigate('구구단');
+        }}
+      />
+    </View>
+  );
+}
+
+/*
+//ch04 lab03 - add 구구단 screen, add image in about
+import React, { useState } from 'react';
+import { Text, View, Button, TextInput, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const text_st = {
+  fontSize: 20,
+  margin: 10,
+};
+
+const input_style = { fontSize: 30, borderWidth: 1, padding: 10, margin: 10 };
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+        <Stack.Screen name="Hello" component={HelloScreen} />
+        <Stack.Screen name="구구단" component={MultiplicationScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function MultiplicationScreen() {
+  let [x, setX] = useState(0);
+  let [y, setY] = useState(0);
+  const [result, setResult] = useState(x * y);
+
+  return (
+    <View style={{ paddingTop: 30 }}>
+      <View style={{ paddingTop: 30 }}>
+        <View style={{ flexDirection: 'row' }}>
+          <Text style={text_st}>
+            {x} X {y} = {result}
+          </Text>
+        </View>
+      </View>
+
+      <View style={{ flexDirection: 'row' }}>
+        <View style={{ flexDirection: 'column' }}>
+          <Button
+            title="  +  "
+            onPress={() => {
+              x++;
+              setX(x);
+              setResult(x * y);
+            }}
+          />
+          <Button
+            title="  -  "
+            onPress={() => {
+              x--;
+              setX(x);
+              setResult(x * y);
+            }}
+          />
+        </View>
+
+        <View style={{ flexDirection: 'column' }}>
+          <Button
+            title="  +  "
+            onPress={() => {
+              y++;
+              setY(y);
+              setResult(x * y);
+            }}
+          />
+          <Button
+            title="  -  "
+            onPress={() => {
+              y--;
+              setY(y);
+              setResult(x * y);
+            }}
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+function HelloScreen() {
+  const [greet, setGreet] = useState('Hello');
+  const [name, setName] = useState('User');
+
+  return (
+    <View style={{ paddingTop: 30 }}>
+      <Text style={text_st}>
+        {greet}, {name}
+      </Text>
+      <TextInput style={input_style} onChangeText={setName} />
+      <View style={{ margin: 10, flexDirection: 'row-reverse' }}>
+        <Button
+          title="Nice"
+          onPress={() => {
+            setGreet('Nice to meet you');
+          }}
+        />
+        <Button
+          title="Hello"
+          onPress={() => {
+            setGreet('Hello');
+          }}
+        />
+      </View>
+    </View>
+  );
+}
+
+function AboutScreen() {
+  return (
+    <View>
+      <Text style={text_st}>This is about the app</Text>
+      <Image
+        style={{ width: 200, height: 200 }}
+        source={require('./assets/cat-icon.png')}
+      />
+    </View>
+  );
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <View>
+      <Text style={text_st}>Home Screen</Text>
+      <Button
+        title="About"
+        onPress={() => {
+          navigation.navigate('About');
+        }}
+      />
+      <View style={{ height: 10 }} />
+      <Button
+        title="Hello"
+        onPress={() => {
+          navigation.navigate('Hello');
+        }}
+      />
+
+      <Button
+        title="구구단"
+        onPress={() => {
+          navigation.navigate('구구단');
+        }}
+      />
+    </View>
+  );
+}
+*/
+
+/*
+//ch04 lab02 - cange Hello --> Hello Ewha
+import React, { useState } from 'react';
+import { Text, View, Button, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const text_st = {
+  fontSize: 20,
+  margin: 10,
+};
+
+const input_style = { fontSize: 30, borderWidth: 1, padding: 10, margin: 10 };
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+        <Stack.Screen name="Hello" component={HelloScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HelloScreen() {
+  const [greet, setGreet] = useState('Hello');
+  const [name, setName] = useState('User');
+
+  return (
+    <View style={{ paddingTop: 30 }}>
+      <Text style={text_st}>
+        {greet}, {name}
+      </Text>
+      <TextInput style={input_style} onChangeText={setName} />
+      <View style={{ margin: 10, flexDirection: 'row-reverse' }}>
+        <Button
+          title="Nice"
+          onPress={() => {
+            setGreet('Nice to meet you');
+          }}
+        />
+        <Button
+          title="Hello"
+          onPress={() => {
+            setGreet('Hello');
+          }}
+        />
+      </View>
+    </View>
+  );
+}
+
+function AboutScreen() {
+  return <Text style={text_st}>This is about the app</Text>;
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <View>
+      <Text style={text_st}>Home Screen</Text>
+      <Button
+        title="About"
+        onPress={() => {
+          navigation.navigate('About');
+        }}
+      />
+      <View style={{ height: 10 }} />
+      <Button
+        title="Hello"
+        onPress={() => {
+          navigation.navigate('Hello');
+        }}
+      />
+    </View>
+  );
+}
+*/
+
+/*
+//ch04 lab01 - Navigating Screens 
+import React from 'react';
+import { Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const text_st = {
+  fontSize: 20,
+  margin: 10,
+};
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+        <Stack.Screen name="Hello" component={HelloScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HelloScreen() {
+  return (
+    <View>
+      <Text style={text_st}>Hello, Navigation</Text>
+      <Button title="Hello" />
+    </View>
+  );
+}
+
+function AboutScreen() {
+  return <Text style={text_st}>This is about the app</Text>;
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <View>
+      <Text style={text_st}>Home Screen</Text>
+      <Button
+        title="About"
+        onPress={() => {
+          navigation.navigate('About');
+        }}
+      />
+      <View style={{ height: 10 }} />
+      <Button
+        title="Hello"
+        onPress={() => {
+          navigation.navigate('Hello');
+        }}
+      />
+    </View>
+  );
+}
+*/
+/*
 // ch03 lab09 - 구구단 using <TextInput>
 
 import React, { useState } from 'react';
@@ -17,8 +470,9 @@ const input_st = {
 };
 
 export default function App() {
-  let [x, setX] = useState(0);
-  let [y, setY] = useState(0);
+  // 함수 안에서만 사용
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
 
   return (
     <View style={{ paddingTop: 30 }}>
@@ -31,6 +485,7 @@ export default function App() {
     </View>
   );
 }
+*/
 
 /*
 // ch03 lab08 - add two numbers
@@ -51,8 +506,8 @@ const input_st = {
 };
 
 export default function App() {
-  let [x, setX] = useState(0);
-  let [y, setY] = useState(0);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
 
   return (
     <View style={{ paddingTop: 30 }}>
@@ -64,8 +519,7 @@ export default function App() {
       </View>
     </View>
   );
-}
-*/
+}*/
 
 /*
 // ch03 lab07 - 구구단 앱
@@ -80,9 +534,9 @@ const text_st = {
 };
 
 export default function App() {
-  let [x, setX] = useState(0);
-  let [y, setY] = useState(0);
-  let [result, setResult] = useState(x * y);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [result, setResult] = useState(x * y);
 
   return (
     <View style={{ paddingTop: 30 }}>
@@ -151,8 +605,8 @@ const text_st = {
 };
 
 export default function App() {
-  let [x, setX] = useState(0);
-  let [y, setY] = useState(0);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
 
   return (
     <View style={{ paddingTop: 30 }}>
@@ -212,7 +666,7 @@ const text_st = {
 };
 
 export default function App() {
-  let [val, setval] = useState(0);
+  const [val, setval] = useState(0);
 
   return (
     <View style={{ paddingTop: 30 }}>
@@ -382,4 +836,5 @@ export default function App() {
     </View>
   );
 }
+
 */
